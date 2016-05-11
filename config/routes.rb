4305be5f66be3b4718356536e9fe01abfc102e6f
Anthony Ho <ahomnky11@gmail.com>
2016-05-11 16:36:54 +0100

    create template, action and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   root 'blog#home'
   get 'about', to: 'blog#about'
+
+  resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
